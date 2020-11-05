46a8da3ba770e8eb484863d2b8db64fa947bfdb6
YOUR FULL NAME <YOUR EMAIL ADDRESS>
2020-11-05 12:03:49 -0800

    final lab 11

--- js/app.js
+++ js/app.js
@@ -1,102 +1,117 @@
-//  create some picture objects
-// will have an array of image objects, and randomly display three  different images on the page
-// we will click on these  to vote
-// we will track our clicks
-// when we hit 25 clicks, remove event listener - close polls
-// when the polls have closed, we render the results
- 
-//    ============ global variables===========           
+'use strict';
 
-var imgContainer = document.getElementById('img-container');
-var imgOneEl = document.getElementById('image-one');
-var imgTwoEl = document.getElementById('image-two');
-var imgThreeEl = document.getElementById('image-three');
-var myList = document.getElementById('list');
+// ========== global variables==========
+
+var pictureOneEl = document.getElementById('picture-one'); // =====images can give properties from the js
+var pictureTwoEl = document.getElementById('picture-two'); // =====images can give properties from the js
+var pictureThreeEl = document.getElementById('picture-three'); // =====images can give properties from the js
+var pictureContener = document.getElementById('picture-contener'); // =====listener wiLL listen to this conainer
+var catalogList = document.getElementById('catalogList'); // =====render result
 
 
-var  pictures = [];  
-var totalClicksAllowed = 25;
-var clicks = 0;
-// number of pictures are displayed. 
-var totalImages = 3;
 
-//================  constructor ============
-function Picture(name){
-    this.name = name;
-    this.src = `img/$(name}.jpg`; 
-    this.views = 0;
-    this.votes = 0;
-    pictures.push(this);
+var totalClickedAllowed = 25;
+var clicked = 0;
+var allPictures = [];
+var renderQueue = [];
 
+function Pictures(name) {
+  this.src = `img/${name}.jpg`;
+  this.name = name;
+  this.votes = 0;
+  this.views = 0;
+  allPictures.push(this);
 }
-   function getRandomPictureIndex(){
-       return Math.floor(Math.random() * Pictures.length);
-   }
+
+function getRandompictures() {
+  return Math.floor(Math.random() * allPictures.length);
+}
+// ==============excuted code===========
+
+new Pictures('bag');
+new Pictures('banana');
+new Pictures('bathroom');
+new Pictures('boots');
+new Pictures('breakfast');
+new Pictures('bubblegum');
+new Pictures('chair');
+new Pictures('cthulhu');
+new Pictures('dog-duck');
+new Pictures('dragon');
+new Pictures('pen');
+new Pictures('pet-sweep');
+new Pictures('scissors');
+new Pictures('shark');
+new Pictures('sweep');
+new Pictures('tauntaun');
+new Pictures('unicorn');
+new Pictures('usb');
+new Pictures('water-can');
+new Pictures('wine-glass');
 
 
-function pictures() {
-new Picture('bag');
-new Picture('banana');
-new Picture('bathroom');
-new Picture('boots');
-new Picture('breakfast');
-new Picture('bubblegum');
-new Picture('chair');
-new Picture('cthulhu');
-new Picture('dog-duck');
-new Picture('dragon');
-new Picture('pen');
-new Picture('pet-sweep');
-new Picture('scissors');
-new Picture('shark');
-new Picture('sweep');
-new Picture('tauntaun');
-new Picture('unicorn');
-new Picture('usb');
-new Picture('water-can');
-new Picture('wine-glass');
-};
 
-function getRandomPictures(){
-    var pictureOne = getRandomPictureIndex();
-    var pictureTwo = getRandomPictureIndex();
-    while (pictureOne=== pictureTwo){
-        pictureTwo= getRandomPictureIndex();
+function populateRenderQueue() {
+  renderQueue = [];
+  while (renderQueue.length < 3) {
+    var uniquePicture = getRandompictures();
+
+    while (renderQueue.includes(uniquePicture)) {
+
+      uniquePicture = getRandompictures();
     }
-    imgOneEl.src = Pictures[PictureOne].src ;
-    imgOneEl.alt = Pictures[PictureOne].name;
-    Pictures[PictureOne].views++;
-    
-    imgTwoEl.src = Pictures[pictureTwo].src;
-    imgTwoEl.alt = Pictures[PictureTwo].name;
-    Pictures[Picturetwo].views++;
-}
-  function renderResult(){
-      for(var i = 0 ;i < Pictures.length; i ++){
-          var li = document.createElement('li');
-          li.textContent = `{pictures[i].name} had ${Pictures[i].votes},and was seen ${Pictures[i].views} times.`;
-           myList.appendChild(li);
-
-      }
+    renderQueue.push(uniquePicture);
   }
-      renderpictures();
-  function handleClick(event){
-      var clickedpicture = event.target.alt;
-      clicks++;
-      
-      for (var i = 0; i < pictures.length; i++){
-          if(clickedpicture=== pictures[i].name){
-            Pictures[i].votes++; 
-          }
-        }
-      
-      if(clicks === totalClicksAllowed){
-        imgContainer.removeEventListener('click', handleClick);
-        
-       
-      }
-    } 
-    renderResult();
-    imgContainer.addEventListener('click',handleClick);
 
-    
+}
+function renderPictures() {
+  populateRenderQueue();
+  var pictureOne = renderQueue[0];
+  var pictureTwo = renderQueue[1];
+  var pictureThree = renderQueue[2];
+
+
+  pictureOneEl.src = allPictures[pictureOne].src;
+  pictureOneEl.alt = allPictures[pictureOne].name;
+  allPictures[pictureOne].views++;
+
+  pictureTwoEl.src = allPictures[pictureTwo].src;
+  pictureTwoEl.alt = allPictures[pictureTwo].name;
+  allPictures[pictureTwo].views++;
+
+  pictureThreeEl.src = allPictures[pictureThree].src;
+  pictureThreeEl.alt = allPictures[pictureThree].name;
+  allPictures[pictureThree].views++;
+}
+function renderResults() {
+  for (var i = 0; i < allPictures.length; i++) {
+    var li = document.createElement('li');
+    li.textContent = `${allPictures[i].name} had ${allPictures[i].votes} votes, and was seen ${allPictures[i].views} times.`;
+    catalogList.appendChild(li);
+
+  }
+
+}
+renderPictures(); //  gives us initial image !!
+
+
+function handleClick(event) {
+  var clickedPictures = event.target.alt;
+  clicked++;
+
+  for (var i = 0; i < allPictures.length; i++) {
+    if (clickedPictures === allPictures[i].name) {
+      allPictures[i].votes++;
+    }
+  }
+  // renderPictures();     //  gives us the images after each click!!
+  renderPictures();
+
+  if (clicked === totalClickedAllowed) {
+
+    pictureContener.removeEventListener('click', handleClick);
+    renderResults();
+  }
+}
+pictureContener.addEventListener('click', handleClick);
+
